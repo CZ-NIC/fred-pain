93e8fc7ce93c780722051e9ce6aa8e5fc6adc0ff
Jan Musílek <jan.musilek@nic.cz>
2019-09-05 13:21:22 +0200

    Ticket #25781 - Update test CI settings

--- setup.py
+++ setup.py
@@ -76,4 +76,5 @@ setup(name='fred-pain',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
       ])
